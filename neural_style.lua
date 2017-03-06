117d38bf53b3e6f063605f953bd27333d3322da8
Valentin <vakons@mail.ru>
2017-03-06 21:15:10 +0300

    Merge "match_colors"

--- neural_style.lua
+++ neural_style.lua
@@ -449,6 +449,59 @@ function original_colors(content, generated)
 end
 
 
+-- from Leon Gatys's code: https://github.com/leongatys/NeuralImageSynthesis/blob/master/ExampleNotebooks/ColourControl.ipynb
+-- and ProGamerGov's code: https://github.com/jcjohnson/neural-style/issues/376
+function match_color(target_img, source_img, mode, eps)
+  -- Matches the colour distribution of the target image to that of the source image
+  -- using a linear transform.
+  -- Images are expected to be of form (c,h,w) and float in [0,1].
+  -- Modes are chol, pca or sym for different choices of basis.
+
+  mode = mode or 'pca'
+  eps = eps or 1e-5
+  local eyem = torch.eye(source_img:size(1)):mul(eps)
+
+  local mu_s = torch.mean(source_img, 3):mean(2)
+  local s = source_img - mu_s:expandAs(source_img)
+  s = s:view(s:size(1), s[1]:nElement())
+  local Cs = s * s:t() / s:size(2) + eyem
+
+  local mu_t = torch.mean(target_img, 3):mean(2)
+  local t = target_img - mu_t:expandAs(target_img)
+  t = t:view(t:size(1), t[1]:nElement())
+  local Ct = t * t:t() / t:size(2) + eyem
+
+  local ts
+  if mode == 'chol' then
+    local chol_s = torch.potrf(Cs, 'L')
+    local chol_t = torch.potrf(Ct, 'L')
+    ts = chol_s * torch.inverse(chol_t) * t
+  elseif mode == 'pca' then
+    local eva_t, eve_t = torch.symeig(Ct, 'V', 'L')
+    local Qt = eve_t * torch.diag(eva_t):sqrt() * eve_t:t()
+    local eva_s, eve_s = torch.symeig(Cs, 'V', 'L')
+    local Qs = eve_s * torch.diag(eva_s):sqrt() * eve_s:t()
+    ts = Qs * torch.inverse(Qt) * t
+  elseif mode == 'sym' then
+    local eva_t, eve_t = torch.symeig(Ct, 'V', 'L')
+    local Qt = eve_t * torch.diag(eva_t):sqrt() * eve_t:t()
+    local Qt_Cs_Qt = Qt * Cs * Qt
+    local eva_QtCsQt, eve_QtCsQt = torch.symeig(Qt_Cs_Qt, 'V', 'L')
+    local QtCsQt = eve_QtCsQt * torch.diag(eva_QtCsQt):sqrt() * eve_QtCsQt:t()
+    ts = torch.inverse(Qt) * QtCsQt * torch.inverse(Qt) * t
+  else
+    assert((mode == 'chol' or
+            mode == 'pca' or
+            mode == 'sym'),
+            'Unknown color matching mode. Stop.')
+  end
+
+  local matched_img = ts:viewAs(target_img) + mu_s:expandAs(target_img)
+  -- matched_img = image.minmax{tensor=matched_img, min=0, max=1}
+  return matched_img:clamp(0, 1)
+end
+
+
 -- Define an nn Module to compute content loss in-place
 local ContentLoss, parent = torch.class('nn.ContentLoss', 'nn.Module')
 
